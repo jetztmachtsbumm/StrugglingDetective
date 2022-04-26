7655b7a4f8dfa613d79da683749bbd3af2ea9dc4
TillFirstmineCoding <till.kloss@gmail.com>
2022-04-26 22:42:29 +0200

    Jump added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,6 +36,8 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Jump() {
+        //der char geht nach oben, habe es leider noch nicht geschafft in wieder landen zu lassen.
+        //versuchte es über einen rigidbody aber da gab es Probleme
         Vector3 direction_up = transform.up * height;
         controller.Move(direction_up * speed * Time.deltaTime);
     }
